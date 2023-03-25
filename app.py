fd76c4a0ff02e65861d464b379ba22918de79e31
Dom Denis <114570732+Domdenis@users.noreply.github.com>
2023-03-25 11:49:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 co = cohere.Client('kxVhnc8p8sLbAYsblZiGAcRi0tPwdoYvYfcXcnkb')
 
-st.title("Module de détection de propos sexiste")
+st.title("Module de détection de propos sexistes")
 st.subheader('sur la base du modele sentiment analysis de co:here')
 st.markdown("Cette application est un module de détection de propos sexiste. Elle est basée sur le modèle de sentiment analysis de co:here. Elle peut être utiliser comme filtre avant la publication sur des forums ou des commentaires.")
 
